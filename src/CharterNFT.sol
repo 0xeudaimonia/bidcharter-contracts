8ca36245adcbe176a769c02afa470c9c7a4bfb18
0xeudaimonia <selectthebest18@gmail.com>
2025-03-12 03:04:14 +0100

    complete charterNFT contract

---
+++ src/CharterNFT.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract CharterNFT is ERC721, ERC721URIStorage, ERC721Burnable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+    uint256 private _nextTokenId;
+
+    constructor(address defaultAdmin, address minter, address burner) ERC721("CharterNFT", "CNFT") {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(MINTER_ROLE, minter);
+        _grantRole(BURNER_ROLE, burner);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://bidcharter.com/nft/";
+    }
+
+    function mint(address to, string memory uri)
+        public
+        onlyRole(MINTER_ROLE)
+        returns (uint256)
+    {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        return tokenId;
+    }
+
+    function burn(uint256 tokenId) public override onlyRole(BURNER_ROLE) {
+        super.burn(tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ test/CharterNFT.t.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import {Test} from "forge-std/Test.sol";
+import {CharterNFT} from "src/CharterNFT.sol";
+import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
+import {IERC721Errors} from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
+
+contract CharterNFTTest is Test {
+    CharterNFT public nft;
+    address public admin;
+    address public minter;
+    address public user;
+    address public burner;
+
+    function setUp() public {
+        admin = makeAddr("admin");
+        minter = makeAddr("minter");
+        user = makeAddr("user");
+        burner = makeAddr("burner");
+        // Deploy contract with admin and minter roles
+        nft = new CharterNFT(admin, minter, burner);
+    }
+
+    function test_InitialSetup() public view {
+        assertTrue(nft.hasRole(nft.DEFAULT_ADMIN_ROLE(), admin));
+        assertTrue(nft.hasRole(nft.MINTER_ROLE(), minter));
+        assertEq(nft.name(), "CharterNFT");
+        assertEq(nft.symbol(), "CNFT");
+    }
+
+    function test_BaseURI() public {
+        uint256 tokenId = 0;
+        vm.expectRevert(abi.encodeWithSelector(
+            IERC721Errors.ERC721NonexistentToken.selector,
+            tokenId
+        ));
+        nft.tokenURI(tokenId);
+    }
+
+    function test_mint() public {
+        // Switch to minter account
+        vm.startPrank(minter);
+        
+        string memory uri = "test-uri";
+        uint256 tokenId = nft.mint(user, uri);
+        
+        assertEq(nft.ownerOf(tokenId), user);
+        assertEq(nft.tokenURI(tokenId), "https://bidcharter.com/nft/test-uri");
+        
+        vm.stopPrank();
+    }
+
+    function test_mintRevertUnauthorized() public {
+        vm.startPrank(user);
+        
+        vm.expectRevert(abi.encodeWithSelector(
+            IAccessControl.AccessControlUnauthorizedAccount.selector,
+            user,
+            nft.MINTER_ROLE()
+        ));
+        nft.mint(user, "test-uri");
+        
+        vm.stopPrank();
+    }
+
+    function test_burn() public {
+        // Mint a token first
+        vm.startPrank(minter);
+        uint256 tokenId = nft.mint(burner, "test-uri");
+        vm.stopPrank();
+
+        // Switch to token owner and burn
+        vm.startPrank(burner);
+        nft.burn(tokenId);
+        
+        // Verify token is burned
+        vm.expectRevert(abi.encodeWithSelector(IERC721Errors.ERC721NonexistentToken.selector, tokenId));
+        nft.ownerOf(tokenId);
+        
+        vm.stopPrank();
+    }
+
+    function test_burnRevertUnauthorized() public {
+        // Mint a token first
+        vm.startPrank(minter);
+        uint256 tokenId = nft.mint(user, "test-uri");
+        vm.stopPrank();
+
+        // Try to burn from unauthorized account
+        address unauthorized = makeAddr("unauthorized");
+        vm.startPrank(unauthorized);
+        
+        vm.expectRevert(abi.encodeWithSelector(
+            IAccessControl.AccessControlUnauthorizedAccount.selector,
+            unauthorized,
+            nft.BURNER_ROLE()
+        ));
+        nft.burn(tokenId);
+        
+        vm.stopPrank();
+    }
+
+    function test_SupportsInterface() public view {
+        // Test for ERC721 interface
+        assertTrue(nft.supportsInterface(0x80ac58cd));
+        // Test for ERC721Metadata interface
+        assertTrue(nft.supportsInterface(0x5b5e139f));
+        // Test for AccessControl interface
+        assertTrue(nft.supportsInterface(0x7965db0b));
+    }
+}
